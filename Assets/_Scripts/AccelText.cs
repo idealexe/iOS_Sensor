573a56c2a64e98adf0204ad013c429a51bc70a3b
idealexe <omega_8810@yahoo.co.jp>
2015-04-29 01:28:09 +0900

    Acceleration

---
+++ Assets/_Scripts/AccelText.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.iOS;
+
+public class AccelText : MonoBehaviour{
+    private Text accelText;
+    private Vector3 acceleration;
+
+	// Use this for initialization
+	void Start () {
+        accelText = gameObject.GetComponent<Text>();
+        acceleration = Vector3.zero;
+
+        drawAcceleration();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        acceleration = Input.acceleration;
+        drawAcceleration();
+
+	
+	}
+
+    private void drawAcceleration()
+    {
+        accelText.text = "Accel X: " + acceleration.x
+            + "\nAccel Y: " + acceleration.y
+            + "\nAccel Z: " + acceleration.z;
+    }
+}
