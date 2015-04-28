a34dce62b814f5c0f9760dba82477b8989299ff8
exe <omega_8810@yahoo.co.jp>
2015-04-28 16:59:54 +0900

    First Commit

---
+++ Assets/_Scripts/Accel_Text.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.iOS;
+
+public class Accel_Text : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
