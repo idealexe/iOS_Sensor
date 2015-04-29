e2f962636d350bc26ebed1276a28358f30e3f4bd
idealexe <omega_8810@yahoo.co.jp>
2015-04-30 01:14:36 +0900

    FixedUpdate

--- Assets/_Scripts/AccelText.cs
+++ Assets/_Scripts/AccelText.cs
@@ -3,29 +3,25 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.iOS;
 
-public class AccelText : MonoBehaviour{
+public class AccelText : MonoBehaviour
+{
     private Text accelText;
     private Vector3 acceleration;
 
-	// Use this for initialization
-	void Start () {
+    void Start()
+    {
         accelText = gameObject.GetComponent<Text>();
         acceleration = Vector3.zero;
+    }
 
-        drawAcceleration();
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        acceleration = Input.acceleration;
-        drawAcceleration();
-
-	
-	}
-
-    private void drawAcceleration()
+    void FixedUpdate()
     {
+        updateAcceleration();
+    }
+
+    private void updateAcceleration()
+    {
+        acceleration = Input.acceleration;
         accelText.text = "Accel X: " + acceleration.x
             + "\nAccel Y: " + acceleration.y
             + "\nAccel Z: " + acceleration.z;
